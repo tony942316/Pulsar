cf4747bb962cac9b73596faf7daa2c79245cdc3b
tony942316 <grassoa159@gmail.com>
2023-08-28 08:16:05 +0000

    Make Static Lib

--- ProvingGrounds/Main.cpp
+++ ProvingGrounds/Main.cpp
@@ -1,8 +1,52 @@
 #include <iostream>
 
+#include "Pulsar/Pulsar.hpp"
+
 int main()
 {
     std::cout << "Start" << std::endl;
+
+    GLFWwindow* window;
+
+    if (!glfwInit())
+    {
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, false);
+
+    window = glfwCreateWindow(640, 480, "Hello Window", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    glViewport(0, 0, 640, 480);
+    glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
     std::cout << "End" << std::endl;
     return 0;
 }
---
+++ include/Pulsar/Headers/details/Dependencies.hpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2023 Anthony H. Grasso
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef PULSAR_DETAILS_DEPENDENCIES_HPP
+#define PULSAR_DETAILS_DEPENDENCIES_HPP
+
+#include "../../Dependencies/Equinox/include/Equinox/Equinox.hpp"
+#include "../../Dependencies/glad/include/glad/glad.h"
+#include "../../Dependencies/glfw/include/GLFW/glfw3.h"
+
+#endif // PULSAR_DETAILS_DEPENDENCIES_HPP
